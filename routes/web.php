09e5013bf42eb56ac604369581c753159a587b14
liuhaichuang <418287613@qq.com>
2020-08-02 07:01:25 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/','StaticPagesController@home');
+Route::get('/','StaticPagesController@home')->name('home');
 
-Route::get('/help','StaticPagesController@help');
+Route::get('/help','StaticPagesController@help')->name('help');
 
-Route::get('/about','StaticPagesController@about');
+Route::get('/about','StaticPagesController@about')->name('about');
 
